99b89f0033d74337f9635301025f36a199ce9683
Manuel Kiangebeni <manuelfuele@icloud.com>
2023-12-29 00:05:59 +0000

    add database config

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import path from 'path'
 import { router } from './routes'
 //-----------------------------------------------
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 //-------------------------------------------------
 
