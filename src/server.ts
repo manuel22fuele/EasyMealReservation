faafe2a81520d58f471fddb9a7e5f5d1f27b75c4
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-01 17:23:18 +0000

    config: build start

--- src/server.ts
+++ src/server.ts
@@ -30,5 +30,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   })
 
 })
-let PORT = 5000
-app.listen(PORT, () => console.log('Servidor online!!!!'))
+
+app.listen(process.env.PORT, () => console.log('Servidor online!!!!'))
