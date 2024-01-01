f7052cead921724ec1f4509125a5897f2c18f2d4
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-01 18:30:13 +0000

    config: build

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,6 @@ module.exports = {
     presets:[
         ["@babel/preset-env",{targets: {node: "current"}}],
         "@babel/preset-typescript"
-    ],
-    "plugins":[
-        "babel-plugin-transform-typescript-metadata",
-        ["@babel/plugin-proposal-decorators",{legacy:true}]
-    ],
+    ]
 
 }
