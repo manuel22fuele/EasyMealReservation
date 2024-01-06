fa1bdff11a01c034963e968b8d32a82e1443b59b
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-06 22:26:22 +0000

    alter table tables

--- src/services/book/ListBookService.ts
+++ src/services/book/ListBookService.ts
@@ -4,9 +4,22 @@ class ListBookService{
     async execute(){
         const ListBook = await prismaClient.book.findMany({
             include:{
-             user:true,
-             tables:true,
-             hours:true
+             user:{
+                select:{
+                    name:true
+                }
+             },
+             tables:{
+                select:{
+                    quantity_people:true,
+                    number_table:true
+                }
+             },
+             hours:{
+                select:{
+                    hour:true
+                }
+             }
             }
         })
         return ListBook;
