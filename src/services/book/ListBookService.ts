b8233ed728a9ef92df3c28b7a014f8e37115fc11
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-06 22:55:13 +0000

    alter table tables

--- src/services/book/ListBookService.ts
+++ src/services/book/ListBookService.ts
@@ -3,6 +3,7 @@ import prismaClient from "../../prisma";
 class ListBookService{
     async execute(){
         const ListBook = await prismaClient.book.findMany({
+            
             include:{
              user:{
                 select:{
@@ -22,7 +23,18 @@ class ListBookService{
              }
             }
         })
-        return ListBook;
+
+        const camposExtraidos = ListBook.map((livro) => ({
+            hour: livro.hours.hour,
+            name: livro.user.name, // Use encadeamento opcional caso 'users' seja indefinido
+            table: {
+              quantity_people: livro.tables.quantity_people,
+              number_table: livro.tables.number_table,
+            },
+          }));
+      
+          return camposExtraidos;
+       
     }
 }
 
