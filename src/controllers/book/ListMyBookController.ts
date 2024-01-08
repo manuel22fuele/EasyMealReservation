acf6d1e064e2bf45a86f6cdc6b4cba5bed1d4398
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-08 21:39:56 +0000

    Added ListMyBook function

--- src/controllers/book/ListMyBookController.ts
+++ src/controllers/book/ListMyBookController.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from "express";
 import { ListMyBookService } from "../../services/book/ListMyBookService";
 
+class ListMyBookController {
+  async handle(req: Request, res: Response) {
+    try {
+      const user_id = req.params.user_id as string;
+      const listBookService = new ListMyBookService();
+      const listBook = await listBookService.execute({ user_id });
 
-class ListMyBookController{
-    async handle(req: Request, res: Response){
-
-        const user_id = req.params.user_id as string
-        const listMyBookController = new ListMyBookService();
-        const ListBook = await listMyBookController.execute({user_id})
-
-        return res.json(ListBook)
+      return res.json(listBook);
+    } catch (error) {
+      return res.status(500).json({ error: "Erro ao listar as reservas" });
     }
+  }
 }
 
-export {ListMyBookController}
+export { ListMyBookController };
