5d782ce844115421836d8a81881fe41f1f69ecb8
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-08 21:28:06 +0000

    Added ListMyBook function

--- src/services/book/ListMyBookService.ts
+++ src/services/book/ListMyBookService.ts
@@ -10,9 +10,32 @@ class ListMyBookService {
       where: {
         user_id: user_id,
       },
-    
+      include: {
+        user: {
+          select: {
+            name: true,
+          },
+        },
+        tables: {
+          select: {
+            quantity_people: true,
+            number_table: true,
+          },
+        },
+        hours: {
+          select: {
+            hour: true,
+          },
+        },
+        dates: {
+          select: {
+            date: true,
+          },
+        },
+      },
     });
-return listBook
+
+    return listBook;
   }
 }
 
