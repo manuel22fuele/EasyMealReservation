a01b7de18abfcf8ed81f810005d12757e619bd03
Manuel Fuele Kiangebeni <manuelfuele@icloud.com>
2024-01-09 00:11:17 +0000

    Added UpdateUser Profile Service

--- src/controllers/book/ListMyBookController.ts
+++ src/controllers/book/ListMyBookController.ts
@@ -5,7 +5,7 @@ import { ListMyBookService } from "../../services/book/ListMyBookService";
 class ListMyBookController{
     async handle(req: Request, res: Response){
 
-        const user_id = req.user_id as string
+        const user_id = req.user_id 
         const listMyBookController = new ListMyBookService();
         const ListMyBook = await listMyBookController.execute(user_id)
 
